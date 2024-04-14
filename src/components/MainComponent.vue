77bf5b70d48e434c464abcf9cfda3daae059f745
abdulhafiz7156 <xxafiz69@gmail.com>
2024-04-14 15:04:20 +0500

    installin font and all images also starting a small part of the project

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebar__home">
+    <div class="sidebar"></div>
+    <div class="main">
+      <div class="container">
+        <h1>  Проведение ТО и мелкий ремонт</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./MainComponent.css"
+export default {
+    name: 'MainComponent',
+}
+</script>
