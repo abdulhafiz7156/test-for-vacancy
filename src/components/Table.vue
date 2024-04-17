3c2d7c56bbe8115fc9be54868ed0b3a0ac8db245
abdulhafiz7156 <xxafiz69@gmail.com>
2024-04-17 14:54:09 +0500

    adding function of changed items posting for server and also addded a few changes on the design

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,7 +3,7 @@
     <table>
       <thead>
       <tr>
-        <th class="id__and__draggable"></th>
+        <th class="id__and__draggable">Номер</th>
         <th class="action">Действие</th>
         <th class="unit-name">Наименование еденицы</th>
         <th class="price">Цена</th>
@@ -12,19 +12,35 @@
         <th class="total">Итого</th>
       </tr>
       </thead>
-      <tbody v-for="(item, index) in this.items" :key="index" draggable="true"
+      <tbody v-for="(item, index) in items" :key="index" draggable="true"
              @dragstart="startDrag(index)" @dragover="overDrag($event)" @drop="handleDrop(index)"
-             @dragend="handleDragEnd" :class="{ 'dragged' : index === this.draggedItem }">
+             :class="{ 'dragged' : index === draggedItem }">
       <tr>
-        <td class="sequence"><img src="../assets/32143241223Combined%20Shape.svg" alt=""> {{ item.id }}</td>
-        <td class="action">
-          <button>Удалить</button>
+        <td class="sequence"><img src="../assets/menu-burger.svg" alt=""> {{ item.id }}</td>
+        <td class="action" @click="openPopup($event)">
+          <img src="../assets/three-dots.svg" alt="">
+          <!-- Popup -->
+          <div v-if="popupVisible" class="popup">
+            <p>Удалить</p>
+            <button @click="deleteItem(index)">Удалить</button>
+          </div>
         </td>
-        <td class="unit-name">{{ item.unitName }}</td>
-        <td class="price">{{ item.price }}</td>
-        <td class="quantity">{{ item.count }}</td>
-        <td class="product-name">{{ item.productName }}</td>
-        <td class="total">{{ item.total }}</td>
+        <td class="unit-name">
+          <input type="text" v-model="item.unitName"  @input="item.edited = true"  >
+        </td>
+        <td class="price">
+          <input type="number" v-model="item.price"   @input="item.edited = true">
+        </td>
+        <td class="quantity">
+          <input type="number" v-model="item.count"  @input="item.edited = true" >
+        </td>
+        <td class="product-name">
+          <input type="text" v-model="item.productName"  @input="item.edited = true" >
+        </td>
+        <td class="total">
+          <input type="number" v-model="item.total"   @input="item.edited = true" >
+        </td>
+        <button v-if="item.edited" @click="saveDataToServer(item)" class="popup-button">Сохранить</button>
       </tr>
       </tbody>
     </table>
@@ -33,7 +49,6 @@
 
 <script>
 export default {
-
   name: "Table-main",
   data() {
     return {
@@ -47,6 +62,7 @@ export default {
           count: 1,
           productName: "Мраморный щебень фр. 2-5 мм, 25кг",
           total: 0,
+          editing: false // Добавлено свойство для редактирования
         },
         {
           id: 2,
@@ -56,6 +72,7 @@ export default {
           count: 2,
           productName: "Мраморный щебень фр. 2-5 мм, 25кг",
           total: 0,
+          editing: false // Добавлено свойство для редактирования
         },
         {
           id: 3,
@@ -65,61 +82,127 @@ export default {
           count: 3,
           productName: "Мраморный щебень фр. 2-5 мм, 25кг",
           total: 0,
+          editing: false // Добавлено свойство для редактирования
         },
       ],
       draggedItem: null,
-    }
+      popupVisible: false,
+      popupPosition: { top: "0", left: "0" },
+    };
   },
   methods: {
     multiplyFunc() {
       this.items.forEach(item => {
-        item.total = item.price * item.count
-      })
+        item.total = item.price * item.count;
+      });
     },
     startDrag(index) {
       this.draggedItem = index;
-      console.log("but start drag: " + index)
+      console.log("but start drag: " + index);
     },
     overDrag(event) {
       event.preventDefault();
       // console.log("but over drag: " + event)
     },
     handleDrop(index) {
-      let droppedItem = this.items.splice(this.draggedItem, 1)[0]
-      this.items.splice(index, 0, droppedItem)
-      this.draggedItem = null
+      let droppedItem = this.items.splice(this.draggedItem, 1)[0];
+      this.items.splice(index, 0, droppedItem);
+      this.draggedItem = null;
       this.items.forEach((item, i) => {
         item.id = i + 1;
       });
-      this.draggedItem = null
+      this.draggedItem = null;
 
-      console.log("but handle drop: " + index)
+      console.log("but handle drop: " + index);
     },
-    handleDragEnd() {
+    deleteItem(index) {
+      this.items.splice(index, 1);
+      this.popupVisible = false; // Close the popup after deletion
     },
+    openPopup(event) {
+      // Get the position of the clicked td element
+      const tdRect = event.currentTarget.getBoundingClientRect();
+
+      // Calculate the top and left position for the popup
+      const popupTop = tdRect.top + window.scrollY + tdRect.height;
+      const popupLeft = tdRect.left + window.scrollX;
+
+      // Update the position of the popup
+      this.popupPosition = {
+        top: popupTop + "px",
+        left: popupLeft + "px",
+      };
+      console.log(popupLeft);
+      this.popupVisible = !this.popupVisible;
+    },
+
+    saveDataToServer(item) {
+      const url = 'https://example/api/'; // Замените на нужный URL вашего API
+
+      const data = {};
+
+      if (item.edited) {
+        data.id = item.id;
+        data.unitName = item.unitName;
+        data.price = item.price;
+        data.count = item.count;
+        data.productName = item.productName;
+        data.total = item.total;
+        // Отправляем AJAX запрос на сервер
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log('Data sent successfully:', data);
+            })
+            .catch(error => {
+              console.error('There was a problem with your fetch operation:', error);
+            });
+
+        item.edited = false;
+      }
+    }
   },
   created() {
-    this.multiplyFunc()
+    this.multiplyFunc();
   },
-
-  components: {}
-}
+};
 </script>
 
+
 <style scoped>
+
+
 .table {
   width: 100%;
   height: 100%;
   margin: 25px 0 175px 0;
-  padding: 9px 1px 25px;
+  padding: 32px 0px 25px;
   border-radius: 10px;
   box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.07);
   background-color: #fff;
+  border: 1px solid #eeeff1;
 }
 
 table {
   width: 100%;
   border-collapse: collapse;
+}
+
+th {
+  border: 1px solid #eeeff1;
+  resize: horizontal; /* Разрешаем изменение размера только по горизонтали */
+  overflow: auto; /* Добавляем полосы прокрутки при изменении размера столбца */
 }
 
 th, td {
@@ -155,11 +238,35 @@ button:hover {
 }
 
 .sequence {
+  display: flex;
   cursor: pointer;
+}
+
+.sequence img {
+  padding-right: 5px;
 }
 
 .dragged {
   border: 2px #a6b7d4 dashed;
   border-radius: 5px;
 }
+
+.popup {
+  position: absolute;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 10px;
+  z-index: 1;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.popup p {
+  margin: 0 0 10px;
+}
+
+.popup-button {
+  margin-left: 10px;
+
+}
+
 </style>
